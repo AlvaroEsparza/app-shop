f6741da70e1808951131fcc52cc7ecdda4202ce9
Alvaro Esparza <alvaro.aer7882@gmail.com>
2018-02-17 01:57:36 -0600

    Controlador de Products y agregar vista de admin para mostrar productos

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,8 +14,26 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-    	factory(Category::class, 5)->create();
+    	/*factory(Category::class, 5)->create();
         factory(Product::class, 100)->create();
         factory(ProductImage::class, 200)->create();
+        */
+
+        $categorias = factory(Category::class, 5)->create();
+        $categorias->each(function ($c){
+            $products= factory(Product::class, 20)->make();
+            $c->products()->saveMany($products);
+
+            $products->each(function ($p){
+                $imagenes = factory(ProductImage::class, 5)->make();
+                $p->images()->saveMany($imagenes);
+            });
+        });
+
+        /*$users = factory(App\User::class, 3)
+           ->create()
+           ->each(function ($u) {
+                $u->posts()->save(factory(App\Post::class)->make());
+            });*/
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,22 +1,10 @@
 @extends('layouts.app')
 
 @section('title', 'Bienvenido a App shop')
-@section('body-class', 'landing-page')
+@section('body-class', 'product-page')
 
 @section('content')
 <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1 class="title">Bienvenido a app shop</h1>
-                <h4>Realiza pedidos emlinea y te contactaremos para la entrega de los mismos.</h4>
-                <br />
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-raised btn-lg">
-                    <i class="fa fa-play"></i>¿Cómo funciona?
-                </a>
-            </div>
-        </div>
-    </div>
 </div>
 
 <div class="main main-raised">
@@ -63,69 +51,51 @@
         </div>
 
         <div class="section text-center">
-            <h2 class="title">Productos disponibles</h2>
+            <h2 class="title">Listado Productos disponibles</h2>
 
             <div class="team">
                 <div class="row">
-                @foreach($products as $product)
-                        <div class="col-md-4">
-                        <div class="team-player">
-                            <img src="{{ $product->images()->first()->image}}" alt="Imagen de producto" class="img-raised img-circle">
-                            <h4 class="title">{{$product->name}} <br />
-                                <small class="text-muted">{{$product->category->name}}</small>
-                            </h4>
-                            <p class="description">{{$product->description}}</p>
-                            <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>
-                            <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-instagram"></i></a>
-                            <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-facebook-square"></i></a>
-                        </div>
-                        </div>
-                @endforeach
+                         <table class="table">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">id</th>
+                                    <th>Nombre</th>
+                                    <th>Descripción</th>
+                                    <th>Categoría</th>
+                                    <th class="text-right">Precio</th>
+                                    <th class="text-right">Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach($products as $product)
+                                <tr>
+                                    <td class="text-center">{{$product->id}}</td>
+                                    <td>{{$product->name}}</td>
+                                    <td class="col-md-3">{{$product->description}}</td>
+                                    <td>{{$product->category->name}}</td>
+                                    <td class="text-right">&dollar; {{$product->price}}</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="View Producto" class="btn btn-info btn-simple btn-xs">
+                                            <i class="fa fa-info"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Editar Producto" class="btn btn-success btn-simple btn-xs">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Eliminar producto" class="btn btn-danger btn-simple btn-xs">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
 
         </div>
 
-
-        <div class="section landing-section">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <h2 class="text-center title">Work with us</h2>
-                    <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
-                    <form class="contact-form">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Your Name</label>
-                                    <input type="email" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Your Email</label>
-                                    <input type="email" class="form-control">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group label-floating">
-                            <label class="control-label">Your Messge</label>
-                            <textarea class="form-control" rows="4"></textarea>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-4 col-md-offset-4 text-center">
-                                <button class="btn btn-primary btn-raised">
-                                    Send Message
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-        </div>
     </div>
 
 </div>
